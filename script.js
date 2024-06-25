7a0f30c3d6aef9bb355e8f676720e1d29e3e168f
Marcus Vinicius Carneiro da Silva <marcusvinicius19112004@gmail.com>
2024-06-25 19:46:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
-/* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2) {
+
+    const opcao1 = "Pedra"
+    const opcao2 = "Papel"
+    const opcao3 = "Tesoura"
+
+    if (player1 == "Pedra" && player2 == "Pedra") {
+
+        return "Empate!"
+
+    }
+
+    else if (player1 == "Pedra" && player2 == "Tesoura") {
+
+        return "Jogador 1 venceu!"
+
+    }
+
+    else if (player1 == "Pedra" && player2 == "Papel") {
+
+        return "Jogador 2 venceu!"
+
+    }
+
+    else if (player1 == "Papel" && player2 == "Papel") {
+
+        return "Empate!"
+
+    }
+
+    else if (player1 == "Papel" && player2 == "Pedra") {
+
+        return "Jogador 1 venceu!"
+
+    }
+
+    else if (player1 == "Papel" && player2 == "Tesoura") {
+
+        return "Jogador 2 venceu!"
+
+    }
+
+    else if (player1 == "Tesoura" && player2 == "Tesoura") {
+
+        return "Empate!"
+
+    }
+
+    else if (player1 == "Tesoura" && player2 == "Papel") {
+
+        return "Jogador 1 venceu!"
+
+    }
+
+    else if (player1 == "Tesoura" && player2 == "Pedra") {
+
+        return "Jogador 2 venceu!"
+
+    }
+}
+
